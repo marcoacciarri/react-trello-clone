e093c49e8f240fc6fd741039ae0c27549a99d1b7
Marco <marco.acciarri@yourzone.it>
2023-06-14 16:53:03 +0200

    create header and add logo

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <Image
+                src="https://yourzone.website/wp-content/uploads/2021/07/favicon-120x120.png"
+                alt="Trello clone"
+                width={120}
+                height={120}
+                className='w-38 pb-10 md:pb-0 object-contain'
+            />
+        </header>
+    )
+}
+
+export default Header
