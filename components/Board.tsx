da4bf813aefb6e7bff92bede74e154558d80bc8e
Marco <marco.acciarri@yourzone.it>
2023-06-19 15:28:12 +0200

    initial Board implementation

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,17 +1,25 @@
 'use client'
 
+import { useBoardStore } from '@/store/BoardStore'
 import React from 'react'
+import { useEffect } from 'react'
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 function Board() {
+    const getBoard = useBoardStore((state) => state.getBoard)
+    useEffect(() => {
+        getBoard();
+    }, [getBoard])
+
     return (
-        <DragDropContext>
-            <Droppable droppableId="" direction="horizontal" type="column">
-                {(provided) => (
-                    <div>{/* rendering columns*/}</div>
-                )}
-            </Droppable>
-        </DragDropContext>
+        <h1>hello</h1>
+        // <DragDropContext>
+        //     <Droppable droppableId="" direction="horizontal" type="column">
+        //         {(provided) => (
+        //             <div>{/* rendering columns*/}</div>
+        //         )}
+        //     </Droppable>
+        // </DragDropContext>
     )
 }
 
