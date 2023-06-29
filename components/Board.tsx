e03d085ab4a162ca4f9c5690eed849ab40ba6aef
Marco <marco.acciarri@yourzone.it>
2023-06-29 13:10:29 +0200

    fix error of missing droppable id

--- components/Board.tsx
+++ components/Board.tsx
@@ -127,7 +127,7 @@ function Board() {
 
     return (
         <DragDropContext onDragEnd={e => handleOnDragEnd}>
-            <Droppable droppableId="" direction="horizontal" type="column">
+            <Droppable droppableId="board" direction="horizontal" type="column">
                 {(provided) => (
                     <div
                         className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
